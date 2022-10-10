5c5f6e4eb128a1d1419a007cb1d2279f38789fe8
KIMYECHAN <kyc5244@gmail.com>
2022-10-10 17:12:18 +0900

    Docs : swagger API Docs 추가

--- routes/coupon.js
+++ routes/coupon.js
@@ -9,4 +9,321 @@ router.get("/", couponController.findCoupons);
 router.patch("/", couponController.updateCoupon);
 router.delete("/:couponCode", couponController.deleteCoupon);
 
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons:
+ *    post:
+ *      summary:  "쿠폰 생성"
+ *      description: "쿠폰을 생성합니다."
+ *      tags: [Coupon]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 생성할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                       {
+ *                           "CouponTypeId" : 5,
+ *                           "state" : "미사용"
+ *                       }
+ *      responses:
+ *        "201":
+ *          description: "생성된 쿠폰 객체를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                           {
+ *                               "id": 11,
+ *                               "coupon_code": "663a0a5c-fc99-457f-9f40-52bedd838a4b",
+ *                               "CouponTypeId": 5,
+ *                               "state": "미사용",
+ *                               "updatedAt": "2022-10-10T06:19:44.706Z",
+ *                               "createdAt": "2022-10-10T06:19:44.706Z"
+ *                           }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons/:couponCode:
+ *    get:
+ *      summary:  "쿠폰 조회"
+ *      description: "쿠폰 코드를 사용해 해당 쿠폰을 조회합니다."
+ *      tags: [Coupon]
+ *      parameters :
+ *         - in : path
+ *           name : couponCode
+ *           required : true
+ *           description : 쿠폰 코드
+ *           schema :
+ *              type : uuid
+ *      responses:
+ *        "200":
+ *          description: "쿠폰 코드에 해당하는 쿠폰 객체를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                           {
+ *                               "id": 11,
+ *                               "coupon_code": "663a0a5c-fc99-457f-9f40-52bedd838a4b",
+ *                               "state": "미사용",
+ *                               "discount_amount": null,
+ *                               "order_num": null,
+ *                               "createdAt": "2022-10-10T06:19:44.000Z",
+ *                               "updatedAt": "2022-10-10T06:19:44.000Z",
+ *                               "OrderId": null,
+ *                               "CouponTypeId": 5,
+ *                               "CouponType": {
+ *                                   "type": "정액 할인"
+ *                               }
+ *                           }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons:
+ *    get:
+ *      summary:  "쿠폰 전체 조회"
+ *      description: "생성된 쿠폰 전체를 조회합니다."
+ *      tags: [Coupon]
+ *      responses:
+ *        "200":
+ *          description: "생성된 쿠폰 전체를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : array
+ *                  example:
+ *                           [
+ *                               {
+ *                                   "id": 2,
+ *                                   "coupon_code": "7b310af8-b06a-474f-be6f-f9e7a9a4019d",
+ *                                   "state": "사용완료",
+ *                                   "discount_amount": "4262",
+ *                                   "order_num": "8d7a344c-cc35-4c80-ba23-7cab7f5cbd84",
+ *                                   "CouponTypeId": 4,
+ *                                   "CouponType": {
+ *                                       "type": "% 할인"
+ *                                   }
+ *                               },
+ *                               {
+ *                                   "id": 4,
+ *                                   "coupon_code": "37e0058c-83d8-4d3c-a18f-46c505e6b0ff",
+ *                                   "state": "사용완료",
+ *                                   "discount_amount": "10000",
+ *                                   "order_num": "12822e18-fddd-4725-8e90-802fda7edfa4",
+ *                                   "CouponTypeId": 5,
+ *                                   "CouponType": {
+ *                                       "type": "정액 할인"
+ *                                   }
+ *                               },
+ *                           ]
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons:
+ *    patch:
+ *      summary:  "쿠폰 수정"
+ *      description: "쿠폰 코드에 해당하는 쿠폰의 데이터를 수정합니다."
+ *      tags: [Coupon]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 수정할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                       {
+ *                           "state": "사용완료",
+ *                           "couponCode":"7b9d046c-9963-45ff-a19a-79b099a27c8c"
+ *                       }
+ *      responses:
+ *        "200":
+ *          description: "쿠폰 정보 수정완료."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                           {
+ *                               "message": "쿠폰 정보 수정 완료"
+ *                           }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons/:couponCode:
+ *    delete:
+ *      summary:  "쿠폰 삭제"
+ *      description: "해당 쿠폰 코드에 해당하는 쿠폰을 삭제합니다."
+ *      tags: [Coupon]
+ *      parameters :
+ *         - in : path
+ *           name : couponCode
+ *           required : true
+ *           description : 쿠폰 코드
+ *           schema :
+ *              type : uuid
+ *      responses:
+ *        "200":
+ *          description: "쿠폰 삭제 완료"
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                          {
+ *                              "message": "쿠폰 삭제 완료"
+ *                          }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/coupons/statistics:
+ *    get:
+ *      summary:  "쿠폰 사용 통계"
+ *      description: "쿠폰 타입 당 총 할인액 및 쿠폰 사용 횟수를 조회합니다."
+ *      tags: [Coupon]
+ *      responses:
+ *        "200":
+ *          description: "쿠폰 타입 당 총 할인액 및 쿠폰 사용 횟수를반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : array
+ *                  example:
+ *                           [
+ *                               {
+ *                                   "CouponTypeId": 4,
+ *                                   "state": "사용완료",
+ *                                   "totalDiscount": 7987,
+ *                                   "count": 2,
+ *                                   "CouponType": {
+ *                                       "type": "% 할인"
+ *                                   }
+ *                               },
+ *                               {
+ *                                   "CouponTypeId": 5,
+ *                                   "state": "사용완료",
+ *                                   "totalDiscount": 20000,
+ *                                   "count": 2,
+ *                                   "CouponType": {
+ *                                       "type": "정액 할인"
+ *                                   }
+ *                               },
+ *                               {
+ *                                   "CouponTypeId": 6,
+ *                                   "state": "사용완료",
+ *                                   "totalDiscount": 6000,
+ *                                   "count": 2,
+ *                                   "CouponType": {
+ *                                       "type": "배송비 할인"
+ *                                   }
+ *                               }
+ *                           ]
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
 module.exports = router;
--- routes/order.js
+++ routes/order.js
@@ -8,4 +8,308 @@ router.delete("/:order_num", orderController.deleteOrder);
 router.get("/:order_num", orderController.findOrder);
 router.get("/", orderController.findOrders);
 
+/**
+ * @swagger
+ * paths:
+ *   /api/orders:
+ *    post:
+ *      summary:  "주문 생성"
+ *      description: "주문 생성 시 원-달러 환율 API를 사용해 배송비를 환전하고 쿠폰 타입에 따라 할인을 제공합니다."
+ *      tags: [Order]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 생성할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                       {
+ *                           "pay_state" : "결제완료",
+ *                           "quantity" : 30,
+ *                           "buyr_city" : "베이징",
+ *                           "buyr_country" : "CN",
+ *                           "buyr_zipx" :  12345,
+ *                           "buyr_name" : "민수",
+ *                           "coupon_code" : "663a0a5c-fc99-457f-9f40-52bedd838a4b"
+ *                       }
+ *      responses:
+ *        "201":
+ *          description: "주문 생성 시 쿠폰을 사용했다면 쿠폰 데이터를 수정하고 생성된 주문 객체를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                           {
+ *                               "delivery_state": "배송대기",
+ *                               "id": 16,
+ *                               "order_num": "ee8815af-d453-40aa-b70a-1784464ed06a",
+ *                               "pay_state": "결제완료",
+ *                               "quantity": 30,
+ *                               "price": "70.63",
+ *                               "buyr_city": "베이징",
+ *                               "buyr_country": "CN",
+ *                               "buyr_zipx": 12345,
+ *                               "buyr_name": "민수",
+ *                               "updatedAt": "2022-10-10T07:37:12.345Z",
+ *                               "createdAt": "2022-10-10T07:37:12.345Z"
+ *                           }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/orders/:order_num:
+ *    get:
+ *      summary:  "주문 조회"
+ *      description: "주문 번호를 사용해 해당 주문을 조회합니다."
+ *      tags: [Order]
+ *      parameters :
+ *         - in : path
+ *           name : order_num
+ *           required : true
+ *           description : 주문번호
+ *           schema :
+ *              type : uuid
+ *      responses:
+ *        "200":
+ *          description: "주문 번호에 해당하는 주문 객체를 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                           {
+ *                               "date": "2022-09-26T07:50:31.000Z",
+ *                               "order_num": "f238486b-bf39-4dcc-9a96-e3b4c404ee60",
+ *                               "pay_state": "결제완료",
+ *                               "delivery_state": "배송중",
+ *                                "quantity": 2,
+ *                               "price": 20.7,
+ *                               "buyr_city": "베이징",
+ *                               "buyr_country": "CN",
+ *                               "buyr_zipx": 12345,
+ *                               "buyr_name": "민수",
+ *                               "delivery_num": "ddd360de-2b33-41b6-8cdd-84cbfca496b0"
+ *                           }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/orders/?start_date&end_date&name&state:
+ *    get:
+ *      summary:  "주문 전체 조회"
+ *      description: "기간별, 주문 상태별로 필터가 가능하고 주문자 명으로 검색이 가능합니다."
+ *      tags: [Order]
+ *      parameters :
+ *         - in : path
+ *           name : start_date
+ *           required : false
+ *           description : 검색 시작 일자
+ *           schema :
+ *              type : String
+ *         - in : path
+ *           name : end_date
+ *           required : false
+ *           description : 검색 끝 일자
+ *           schema :
+ *              type : String
+ *         - in : path
+ *           name : name
+ *           required : false
+ *           description : 주문자 이름
+ *           schema :
+ *              type : String
+ *         - in : path
+ *           name : state
+ *           required : false
+ *           description : 주문 상태
+ *           schema :
+ *              type : String
+ *      responses:
+ *        "200":
+ *          description: "조건에 해당하는 주문 객체들을 반환합니다."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : array
+ *                  example:
+ *                           [
+ *                               {
+ *                                   "id": 2,
+ *                                   "date": "2022-09-26T07:50:31.000Z",
+ *                                   "order_num": "f238486b-bf39-4dcc-9a96-e3b4c404ee60",
+ *                                   "pay_state": "결제완료",
+ *                                   "delivery_state": "배송중",
+ *                                   "quantity": 2,
+ *                                   "price": 20.7,
+ *                                   "buyr_name": "민수",
+ *                                   "delivery_num": "ddd360de-2b33-41b6-8cdd-84cbfca496b0"
+ *                               },
+ *                               {
+ *                                   "id": 4,
+ *                                   "date": "2022-09-26T08:21:12.000Z",
+ *                                   "order_num": "92c81880-3607-4aed-b10e-c042b4da31ef",
+ *                                   "pay_state": "결제대기",
+ *                                   "delivery_state": "배송대기",
+ *                                   "quantity": 3,
+ *                                   "price": 45.96,
+ *                                   "buyr_name": "민수",
+ *                                   "delivery_num": null
+ *                               },
+ *                               {
+ *                                   "id": 6,
+ *                                   "date": "2022-09-29T02:03:19.000Z",
+ *                                   "order_num": "806e025d-1965-4024-97f9-dadd14d5e768",
+ *                                   "pay_state": "결제완료",
+ *                                   "delivery_state": "배송대기",
+ *                                   "quantity": 2,
+ *                                   "price": 26.77,
+ *                                   "buyr_name": "민수",
+ *                                   "delivery_num": null
+ *                               },
+ *                           ]
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/orders:
+ *    patch:
+ *      summary:  "주문 수정"
+ *      description: "주문 번호에 해당하는 주문의 데이터를 수정합니다. 배송중, 배송완료라면 송장번호가 추가됩니다."
+ *      tags: [Order]
+ *      parameters :
+ *         - in : body
+ *           name : data
+ *           required : true
+ *           description : 수정할 데이터
+ *           schema :
+ *              type : object
+ *              example :
+ *                       {
+ *                           "order_num" : "c9e390a3-3f98-4f04-9390-8e8efc6208ca",
+ *                           "pay_state" : "결제완료",
+ *                           "delivery_state" : "배송대기",
+ *                           "buyr_city" : "베이징",
+ *                           "buyr_country" : "CN",
+ *                           "buyr_zipx" :  12345,
+ *                           "buyr_name" : "민정"
+ *                       }
+ *      responses:
+ *        "200":
+ *          description: "주문 배송 수정 완료"
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                          { message: "주문 배송 상태 수정 완료" }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
+/**
+ * @swagger
+ * paths:
+ *   /api/orders/:order_num:
+ *    delete:
+ *      summary:  "주문 취소"
+ *      description: "주문 번호에 해당하는 주문을 취소합니다."
+ *      tags: [Order]
+ *      parameters :
+ *         - in : path
+ *           name : order_num
+ *           required : true
+ *           description : 주문 번호
+ *           schema :
+ *              type : uuid
+ *      responses:
+ *        "200":
+ *          description: "주문 취소 완료."
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  type : object
+ *                  example:
+ *                          {
+ *                              "message": "주문 취소 완료"
+ *                          }
+ *        "400":
+ *          description: Bad request
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                example :
+ *                  {
+ *                    error:
+ *                        {
+ *                           message: error.message,
+ *                           field: error.name
+ *                        }
+ *                  }
+ *
+ */
+
 module.exports = router;
